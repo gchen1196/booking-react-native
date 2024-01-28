5a923090b98d6ab20498a828f7c3b5e0335a588b
George Chen <gchen@zoiclabs.com>
2024-01-28 09:42:52 -0800

    import and use custom fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,38 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
+import { useCallback } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+//prevents SplashScreen from auto hiding while loading font
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  /**
+   * NOTE: expo-font to load fonts at runtime: https://docs.expo.dev/versions/latest/sdk/font/
+   */
+  const [fontsLoaded, fontError] = useFonts({
+    regular: require('./assets/fonts/regular.otf'),
+    medium: require('./assets/fonts/medium.otf'),
+    bold: require('./assets/fonts/bold.otf'),
+    light: require('./assets/fonts/light.otf'),
+    xtrabold: require('./assets/fonts/xtrabold.otf'),
+  });
+
+  //after fonts been loaded, can hide splash screen
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +45,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily: 'bold',
+    fontSize: 24
+  }
 });
